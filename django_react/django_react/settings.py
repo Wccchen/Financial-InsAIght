d10527c70c1dadb2154136772b5b5c608006e5ef
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-07-07 02:26:52 +0100

    Update

--- django_react/django_react/settings.py
+++ django_react/django_react/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend',
+    'backend.apps.BackendConfig',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
