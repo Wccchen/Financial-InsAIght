0aba3f9d9ddc54e23fae7aae5f1af150e7a82a11
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-07-16 01:13:56 +0100

    REST framwork initialise

--- django_react/backend/models.py
+++ django_react/backend/models.py
@@ -15,6 +15,6 @@ def generate_unique_code():
 class Room(models.Model):
     code = models.CharField(max_length=8, default="", unique=True)
     host = models.CharField(max_length=50, unique=True)
-    guess_can_pause = models.BooleanField(null=False, default=False)
+    guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
     created_at = models.DateTimeField(auto_now_add=True)
