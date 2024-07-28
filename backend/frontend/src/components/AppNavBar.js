9eeac0f0d47b96c899863f29e41609031334dbd6
Wccchen <149198631+Wccchen@users.noreply.github.com>
2024-07-28 20:55:27 +0100

    Setting up the login system

---
+++ backend/frontend/src/components/AppNavBar.js
@@ -0,0 +1,79 @@
+import { Avatar, Dropdown, Navbar } from "flowbite-react";
+import UserIcon from "../images/user.png";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+const AppNavBar = (props) => {
+  let navigate = useNavigate();
+
+  const { isLoggedIn, setIsLoggedIn, name, setName, email, setEmail } = props;
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    setName(null);
+    setEmail(null);
+    navigate("/");
+    toast.success("You are successfully logged out!");
+  };
+
+  return (
+    <Navbar fluid>
+      <Navbar.Brand href="#">
+        <img
+          src="https://media.geeksforgeeks.org/wp-content/uploads/20210224040124/JSBinCollaborativeJavaScriptDebugging6-300x160.png"
+          className="mr-3 h-6 sm:h-9"
+          alt="Flowbite React Logo"
+        />
+        <span className="self-center whitespace-nowrap text-3xl font-semibold dark:text-white">
+          GeeksForGeeks
+        </span>
+      </Navbar.Brand>
+      {isLoggedIn && (
+        <div className="flex md:order-2">
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={<Avatar alt="User settings" img={UserIcon} rounded />}
+          >
+            <Dropdown.Header>
+              <span className="block text-sm">{name}</span>
+              <span className="block truncate text-sm font-medium">
+                {email}
+              </span>
+            </Dropdown.Header>
+            <Dropdown.Item>Settings</Dropdown.Item>
+            <Dropdown.Item>Your Orders</Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item onClick={handleLogout}>Log out</Dropdown.Item>
+          </Dropdown>
+          <Navbar.Toggle />
+        </div>
+      )}
+      <Navbar.Collapse>
+        <Navbar.Link href="/" className="text-lg">
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-lg">
+          About
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-lg">
+          Services
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-lg">
+          Pricing
+        </Navbar.Link>
+        <Navbar.Link href="#" className="text-lg">
+          Contact
+        </Navbar.Link>
+        {!isLoggedIn && (
+          <Navbar.Link href="/login" className="text-lg">
+            Login
+          </Navbar.Link>
+        )}
+      </Navbar.Collapse>
+    </Navbar>
+
+  );
+};
+
+export default AppNavBar;
